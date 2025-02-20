cabc8de466878b436af77c19f69d776ce7eac52f
archit dharod <architdharod@Mac.fritz.box>
2025-02-20 21:46:53 +0100

    added startup shell script to install stow and execute stow commands

--- install.sh
+++ install.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
+set -e
 
-sudo apt update
-sudo at install -y stow
+# Function to install GNU Stow on macOS using Homebrew
+install_stow_macos() {
+    echo "Detected macOS."
+    echo "Installing GNU Stow with Homebrew..."
+    brew install stow
+}
 
-stow nvim
+# Function to install GNU Stow on Debian/Ubuntu-based systems
+install_stow_debian() {
+    echo "Detected Debian/Ubuntu-based Linux."
+    sudo apt-get update
+    sudo apt-get install -y stow
+}
+
+# Function to install GNU Stow on Red Hat-based systems (if needed)
+install_stow_redhat() {
+    echo "Detected Red Hat-based Linux."
+    sudo yum install -y stow
+}
+
+# Detect operating system and install GNU Stow accordingly
+OS_TYPE=$(uname)
+if [ "$OS_TYPE" = "Darwin" ]; then
+    install_stow_macos
+elif [ "$OS_TYPE" = "Linux" ]; then
+    if [ -f /etc/debian_version ]; then
+        install_stow_debian
+    elif [ -f /etc/redhat-release ]; then
+        install_stow_redhat
+    else
+        echo "Linux distribution not specifically supported. Please install GNU Stow manually."
+        exit 1
+    fi
+else
+    echo "Unsupported OS: $OS_TYPE"
+    exit 1
+fi
+
+stow -v nvim
+
+FILE=~/.tmux.conf
+if [ -f "$FILE" ]; then
+  echo "Removing existing .tmux.conf"
+  rm ~/.tmux.conf
+fi
+
+echo "Stowing .tmux.conf"
+stow -v tmux
+
+echo "Dotfiles successfully stowed!"
--- nvim/.config/nvim/README.md
+++ nvim/.config/nvim/README.md
@@ -1,3 +1,5 @@
+
+
 ## Automations for Windows:
 ### jump to the nvim config directory:
 1. Create a file named "Microsoft.PowerShell_profile.ps1" under the following folder: "C:\Users\[user-name]\Documents\WindowsPowerShell"
